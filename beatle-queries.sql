db3a403bea8aaa0860131b81fa2bb2a371f5e25c
Lexi Sharp <121321500+HASharp29@users.noreply.github.com>
2023-11-02 21:50:44 -0400

    Update beatle-queries.sql

--- beatle-queries.sql
+++ beatle-queries.sql
@@ -17,3 +17,26 @@ FROM Measure
 SELECT *
 FROM Marker
 ;
+
+-- Get all of the measures of a specific click track in order
+SELECT *
+FROM Measure
+WHERE clickTrackID =3
+ORDER BY measureNum
+;
+
+-- get all of the click tracks of a sepcific user ordered by date
+SELECT *
+FROM clickTrack
+WHERE userID = 1
+ORDER BY date
+;
+
+-- get a list of users of a specific user
+SELECT username
+FROM theUser
+
+-- get the password of a specific user
+SELECT password
+FROM theUser
+WHERE userID = 3
