80b42fcba2e9c54a28fe3e6bd507230df2967cad
KazFU <caden9293@gmail.com>
2023-11-03 14:19:53 -0400

    Def didn't just copy the profs code.

---
+++ beatleService.js
@@ -0,0 +1,37 @@
+// Set up the database connection.
+
+const pgp = require('pg-promise')();
+const db = pgp({
+    host: process.env.DB_SERVER,
+    port: process.env.DB_PORT,
+    database: process.env.DB_USER,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
+});
+
+// Configure the server and its routes.
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const router = express.Router();
+router.use(express.json());
+
+router.get("/", readHelloMessage);
+
+app.use(router);
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+// Implement the CRUD operations.
+
+function returnDataOr404(res, data) {
+    if (data == null) {
+        res.sendStatus(404);
+    } else {
+        res.send(data);
+    }
+}
+
+function readHelloMessage(req, res) {
+    res.send('Sup motherfuckers!');
+}
