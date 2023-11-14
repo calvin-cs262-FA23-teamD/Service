2b5c8c304aa1ef66465735542edfc9bc3ad84cc6
Kaz <128867721+KazFU@users.noreply.github.com>
2023-11-13 21:51:05 -0500

    Update beatleService.js

--- beatleService.js
+++ beatleService.js
@@ -17,7 +17,7 @@ const port = process.env.PORT || 3000;
 const router = express.Router();
 router.use(express.json());
 
-router.get("/", readHelloMessage);
+router.get("/test", readHelloMessage);
 
 app.use(router);
 app.listen(port, () => console.log(`Listening on port ${port}`));
