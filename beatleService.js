465837d65ad72dbe24dba03aca8ba25822306556
Kaz <128867721+KazFU@users.noreply.github.com>
2023-11-14 21:11:44 -0500

    Update beatleService.js

--- beatleService.js
+++ beatleService.js
@@ -1,47 +1,23 @@
-/* eslint-disable no-template-curly-in-string */
-/* eslint-disable no-console */
-/* eslint-disable no-use-before-define */
-/**
- * This module implements a REST-inspired webservice for the Beatle DB.
- * The database is hosted on ElephantSQL.
- *
- * To guard against SQL injection attacks, this code uses pg-promise's built-in
- * variable escaping. This prevents a client from issuing this URL:
- *     https://cs262-webservice.azurewebsites.net//players/1%3BDELETE%20FROM%20PlayerGame%3BDELETE%20FROM%20Player
- * TODO: Consider using Prepared Statements.
- *      https://vitaly-t.github.io/pg-promise/PreparedStatement.html
- *
- * This service assumes that the database connection strings and the server mode are
- * set in environment variables. See the DB_* variables used by pg-promise. And
- * setting NODE_ENV to production will cause ExpressJS to serve up uninformative
- * server error responses for all errors.
- *
- * @author: kvlinden
- * @date: Summer, 2020
- */
-
 // Set up the database connection.
 
 const pgp = require('pg-promise')();
-
 const db = pgp({
-  host: process.env.DB_SERVER,
-  port: process.env.DB_PORT,
-  database: process.env.DB_USER,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+    host: process.env.DB_SERVER,
+    port: process.env.DB_PORT,
+    database: process.env.DB_USER,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
 });
 
 // Configure the server and its routes.
 
 const express = require('express');
-
 const app = express();
 const port = process.env.PORT || 3000;
 const router = express.Router();
 router.use(express.json());
 
-router.get('/', readHelloMessage);
+router.get("/", readHelloMessage);
 
 app.use(router);
 app.listen(port, () => console.log(`Listening on port ${port}`));
@@ -49,13 +25,13 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
 // Implement the CRUD operations.
 
 function returnDataOr404(res, data) {
-  if (data == null) {
-    res.sendStatus(404);
-  } else {
-    res.send(data);
-  }
+    if (data == null) {
+        res.sendStatus(404);
+    } else {
+        res.send(data);
+    }
 }
 
 function readHelloMessage(req, res) {
-  res.send('Hello, CS 262 Beatle service!');
+    res.send('Sup');
 }
