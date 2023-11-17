da34a18ce3779a850b26222fdce33080eae3cf32
KazFU <caden9293@gmail.com>
2023-11-17 14:41:34 -0500

    fuck azure

--- beatleService.js
+++ beatleService.js
@@ -50,7 +50,7 @@ function readallUsers(req, res, next) {
 }
 
 function readtheUser(req, res, next) {
-  db.oneOrNone('SELECT * FROM theUser WHERE id=${id};', req.params)
+  db.oneOrNone('SELECT * FROM theUser WHERE id=${id}', req.params)
     .then((data) => {
       returnDataOr404(res, data);
     })
