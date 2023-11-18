d42e84895c50c3ce3ffc280efddfd04ce95002b2
KazFU <caden9293@gmail.com>
2023-11-18 18:55:18 -0500

    Started eslint work for beatleService

--- beatleService.js
+++ beatleService.js
@@ -1,26 +1,25 @@
+/* eslint-disable no-template-curly-in-string */
 // Set up the database connection.
 
 // Using key-value pairs from ElephantSQL
 const pgp = require('pg-promise')();
+
 const db = pgp({
   host: process.env.DB_SERVER,
   port: process.env.DB_PORT,
   database: process.env.DB_USER,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD
+  password: process.env.DB_PASSWORD,
 });
 
 // Configure the server and its routes.
 
 const express = require('express');
+
 const app = express();
 const port = process.env.PORT || 3000;
 const router = express.Router();
 router.use(express.json());
-
-router.get("/", readHelloMessage);
-router.get('/allUsers', readallUsers);
-router.get('/theUser/:id', readtheUser);
 
 app.use(router);
 app.listen(port, () => console.log(`Listening on port ${port}`));
@@ -58,3 +57,7 @@ function readtheUser(req, res, next) {
       next(err);
     });
 }
+
+router.get('/', readHelloMessage);
+router.get('/allUsers', readallUsers);
+router.get('/theUser/:id', readtheUser);
