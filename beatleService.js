811fb06d66f091453dbe5642cbb7c3ecdffbd1a3
KazFU <caden9293@gmail.com>
2023-12-14 10:14:27 -0500

    Update 11

--- beatleService.js
+++ beatleService.js
@@ -154,9 +154,9 @@ function readaClickTrack(req, res, next) {
 
 function readallClickTracksFromUser(req, res, next) {
   const id = req.params.id; // Assuming ID is in the 'id' parameter
-  db.oneOrNone('SELECT clickTrack.* FROM clickTrack INNER JOIN theUser ON clickTrack.userID = theUser.ID WHERE theUser.ID = $1 ORDER BY clickTrack.name ASC;', [id])
+  db.oneOrNone('SELECT clickTrack.* FROM clickTrack LEFT JOIN theUser ON clickTrack.userID = theUser.ID WHERE theUser.ID = $1 ORDER BY clickTrack.name ASC;', [id])
     .then((data) => {
-      returnDataOr404(res, data);
+      returnDataOr404(res, data);/
     })
     .catch((err) => {
       next(err);
